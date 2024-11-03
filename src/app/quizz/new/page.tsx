761e64ae917b426d9794e01cdd0b17c76f1ea313
Arkashersz <extensed.wc@hotmail.com>
2024-11-03 13:38:04 -0300

    feat: Upload and parse the document

---
+++ src/app/quizz/new/page.tsx
@@ -0,0 +1,14 @@
+import UploadDoc from "../UploadDoc";
+
+const page = () => {
+    return (
+        <div className="flex flex-col flex-1">
+            <main className="py-11 flex flex-col text-center gap-4 items-center flex-1 mt-24">
+                <h2 className="text-3xl font-bold mb-4">Qual será o tema do Quiz de hoje?</h2>
+                <UploadDoc />
+            </main>
+        </div>
+    )
+}
+
+export default page;
