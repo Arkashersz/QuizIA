fb04419107c580b0aeb02e0536c82188f73a351d
Arkashersz <extensed.wc@hotmail.com>
2024-11-05 10:37:14 -0300

    feat: AI Integration

--- src/app/quizz/UploadDoc.tsx
+++ src/app/quizz/UploadDoc.tsx
@@ -5,11 +5,29 @@ import { Button } from "@/components/ui/button";
 const UploadDoc = () => {
   const [document, setDocument] = useState<Blob | File | null | undefined>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!document) {
+      setError("Por favor, arrasque o arquivo ou faça upload antes!");
+      return
+    }
     setIsLoading(true);
-    console.log(document);
+    const formData = new FormData();
+    formData.append("pdf", document as Blob);
+    try {
+      const res = await fetch("/api/quizz/generate", {
+        method: "POST",
+        body: formData
+      });
+      if (res.status === 200) {
+        console.log("quizz generated sucessfuly");
+      }
+    } catch (e) {
+      console.log("error while generating", e);
+    }
+    setIsLoading(false);
   };
   return (
     <div className="w-full">
@@ -31,6 +49,7 @@ const UploadDoc = () => {
             onChange={(e) => setDocument(e?.target?.files?.[0])}
           />
         </label>
+        {error ? <p className="text-red-600">{error}</p> : null}
         <Button size="lg" className="mt-2" type="Submit">Gerar Quiz 🖋️</Button>
       </form>
     </div>
